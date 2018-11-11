0798e1fdd404fc105bfeaf3706e87fc2964fff51
Ville Heikkilä <ville.heikkila@helsinki.fi>
2018-11-11 17:25:14 +0200

    maksukortti testit 100%

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,27 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void rahanLatausToimii() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenToimii() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+    }
+    
+    @Test
+    public void rahanOttaminenEiNegatiivinen() {
+        kortti.otaRahaa(11);
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test
+    public void saldoOikeaArvo() {
+        assertEquals(10, kortti.saldo());
+    }
 }
