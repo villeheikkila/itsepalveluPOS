684d92fd479ea99807282777cd5ce57cb2fe5fea
Ville Heikkilä <ville.heikkila@helsinki.fi>
2018-11-20 03:16:22 +0200

    Lisätään jonkunlainen testi, pitää muokata paremmaksi. Toistaiseksi vähän epäselvää, miten tätä pitäisi testata

--- src/test/java/itsepalvelupos/domain/StoreTest.java
+++ src/test/java/itsepalvelupos/domain/StoreTest.java
@@ -4,6 +4,7 @@ import itsepalvelupos.database.Database;
 import itsepalvelupos.database.ProductDao;
 import itsepalvelupos.domain.Product;
 import itsepalvelupos.domain.Store;
+import java.io.File;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +12,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StoreTest {
 
+
     public StoreTest() {
     }
 
     @Test
-    void addProduct() {
-
+    void addProductActuallyAddsProduct()  throws Exception{
+        File file = new File("test.db");
+        Database database = new Database("test.db");
+        database.databaseInit();
+        Store store = new Store(1000);
+        store.addProduct(database, "Pepsi", 5, 150);
+        ProductDao product = new ProductDao(database);
+        Product test = product.findOne(1);
+        assertEquals(test.getName(), "Pepsi");
+        assertEquals(test.getInventory(), 150);
+        assertEquals(test.getPrice(), 5);
+        file.delete();
     }
 
     @Test
